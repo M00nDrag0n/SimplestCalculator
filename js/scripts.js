c8a79ae876cac68d4b9d5fe1fdc61e7871b1cf14
Anise Lester and Jonathan Salfen <anise@>
2019-10-21 16:50:21 -0700

    save JavaScript changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,47 @@
-//$(document).ready(function(() {
-
-   //$(".profile").click(function(event) {
+// $(document).ready(function() {
+//
+//    $(".profile").click(function(event) {
 
 
 function clickME(){
 
+  function lillianDialogue() {
+    alert("Gather round children, and test your NUMERICAL MIGHT against my ~mystical equations~!");
+    alert("Each is designed to STUPIFY the mind, and ~perplex~ the SOOOOOUUUUULLLL");
+    var num = parseInt(prompt("BEGIN!!!!!!!! [2 + 2] If you dare...."));
+    if (num == 4) {
+      alert("WHAT??? IMPOSSIBLE!! NNOOOOOOOO!!!!!!")
+      // $(".stuff").hide();
+      // $(".exp").toggle();
+
+    } else {
+      alert("You fool! You have been defeated! Come back again when you have learned more...")
+    }
+  }
      var number1 = parseInt(prompt("FEED ME YOUR NUMBERS"));
-     var number2 = parseInt(prompt("I NEED MORE!!!!!!!!!:"));
+     var number2 = parseInt(prompt("I NEED MORE!!!!!!!!!"));
      var result = (number1 + number2);
      alert("MY TOTAL STOMACH CONTENTS ARE: " + result + " Numbers");
      alert("I SPEW VENOM AT YOUUUU");
      alert("BLEHHHHHH!!!!")
      prompt("I BITE!!")
+     alert("HAHAHAHAHAHAHA!!!!")
 
  }
 
 
 
+function extraSpeech() {
+var number1 = parseInt(prompt("FEED ME! Commas are T0XiC and So are Other symbols! ANYTHING BUT THOSE!!!"));
+var etc = (prompt("WHAT ARE THESE!!!"));
+var result2 = (number1 +  etc);
+alert("MY TOTAL STOMACH CONTENTS ARE: " + result2);
+alert("THANK YOU");
+alert("I WILL FOLLOW YOU HOME AND EAT ALL OF YOUR " + etc)
+}
 
-
-
-
-
+// });
+// });
 
 // // var clac = prompt("YOU WILL GIVE ME FOOD??!!!"") {
 // //
