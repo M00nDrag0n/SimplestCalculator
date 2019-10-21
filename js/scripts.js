4c14add8ed9c1737c5e3a4ba1fc8e6f5a3003d29
Anise Lester and Jonathan Salfen <jonathan@>
2019-10-21 15:42:31 -0700

    add a whole bunch of stuff all at once

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+//$(document).ready(function(() {
+
+   //$(".profile").click(function(event) {
+
+
+function clickME(){
+
+     var number1 = parseInt(prompt("FEED ME YOUR NUMBERS"));
+     var number2 = parseInt(prompt("I NEED MORE!!!!!!!!!:"));
+     var result = (number1 + number2);
+     alert("MY TOTAL STOMACH CONTENTS ARE: " + result + " Numbers");
+     alert("I SPEW VENOM AT YOUUUU");
+     alert("BLEHHHHHH!!!!")
+     prompt("I BITE!!")
+
+ }
+
+
+
+
+
+
+
+
+
+// // var clac = prompt("YOU WILL GIVE ME FOOD??!!!"") {
+// //
+// // };
+// //
+// // function speech2 = alert("FEED ME WITH NUMBERS SO I CAN SPEW VENOM IN YOUR DIRECTION!!!! HAHAHAHAHAHAHAHAAHHHHHHH")
+// //
+// // prompt()
+//
+// // if else function, jquery, hover .show(), .hide();
+// });
